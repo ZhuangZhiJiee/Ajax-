9715d356aeadc9bcce9fad6e58d403e2311cc0cf
莊智傑 <chaosabyss73@gmail.com>
2022-10-03 21:45:19 +0800

    加入專案檔案。

---
+++ Ajax作業/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ajax作業.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
